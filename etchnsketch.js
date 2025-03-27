12dddf4f951e8a6bb31fb71b77de887eb593ccab
tubusor <160207643+Tobitza@users.noreply.github.com>
2025-03-28 00:12:32 +0200

    Added resetting and on-hover coloring

--- etchnsketch.js
+++ etchnsketch.js
@@ -1,13 +1,44 @@
 const container = document.querySelector('#flex-container');
+const resetButton = document.querySelector('#grid-reset');
 
-const gridSize = 16;
-const totalSquares = gridSize * gridSize;
+let gridSize = 16;
+// let totalSquares = gridSize * gridSize;
 
-//Generating the squares
-// const divArray = [];
-for (let i=0; i < totalSquares; i++){
-    const square = document.createElement("div");
-    square.classList.add("grid-square");
-    container.appendChild(square)
-    // divArray.push(square);
+generateGrid(gridSize);
+
+resetButton.addEventListener("click", () => {
+    newGridSize = parseInt(prompt("Enter the number of squares:"));
+    newTotalSquares = newGridSize * newGridSize;
+
+    resetGrid();
+    generateGrid(newTotalSquares);
+});
+
+function resetGrid() {
+    container.innerHTML = "";
+}
+
+function generateGrid(argSquares) {
+    document.documentElement.style.setProperty("--grid-size", argSquares);
+
+    let argTotalSquares = argSquares * argSquares;
+    for (let i=0; i < argTotalSquares; i++){
+        const square = document.createElement("div");
+        square.classList.add("grid-square");
+
+        // Hover effect <- Maybe move this to a function TBH
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "blue";
+        });
+
+        // Hover effect TEST with fading
+        // square.addEventListener("mouseenter", () => {
+        //     square.style.backgroundColor = "blue";
+        //     setTimeout(() => {
+        //         square.style.backgroundColor = "";
+        //     }, 500);
+        // });
+
+        container.appendChild(square);
+    }
 }
